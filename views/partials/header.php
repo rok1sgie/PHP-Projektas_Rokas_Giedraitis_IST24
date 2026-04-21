908ce305df1644c88cdae78a035043eb1621f2e1
rok1sgie <rokas.giedraitis@stud.svako.lt>
2026-04-21 21:08:37 +0300

    Header ir footer

---
+++ views/partials/header.php
@@ -0,0 +1,11 @@
+<?php require_once __DIR__ . '/../../config/config.php'; ?>
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slaptažodžių saugykla</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+<body>
+<div class="container">
